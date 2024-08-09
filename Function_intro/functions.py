ea70dcecee2d8d2bb4ebee5a00d3f1c9364ae3ec
Shantanu Ray <rayshantanu.23@gmail.com>
2024-08-09 13:16:36 +0530

    Adding the folder Function_intro

---
+++ Function_intro/functions.py
@@ -0,0 +1,7 @@
+def multiply(e, f):
+    d = e * f
+    return d
+
+for val in range(1,11):
+    answer = multiply(2, val)
+    print(answer)
---
+++ Function_intro/readinfile.py
@@ -0,0 +1,5 @@
+text = open('Text_File.txt','r')
+for line in text:
+    print(line)
+
+text.close()
